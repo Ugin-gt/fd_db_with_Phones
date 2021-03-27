166e119388e923d19d3cd57411550efdb2c11be3
Ugin-gt <apach575@gmail.com>
2021-03-27 03:51:54 +0200

    Add new structure

--- fd_db_shipment.sql
+++ fd_db_shipment.sql
@@ -1,45 +1,45 @@
-
 CREATE TABLE "customer"(
-  id serial PRIMARY KEY,
-  name varchar(128) NOT NULL,
-  adress JSONB NOT NULL,
-  email varchar(256) NOT NULL CHECK (email != ''),
-  phone char(11) NOT NULL CHECK (phone != '')
-  );
-
+  "id" serial PRIMARY KEY,
+  "name" varchar(128) NOT NULL,
+  "adress" JSONB NOT NULL,
+  "email" varchar(256) NOT NULL CHECK (email != ''),
+  "phone" char(11) NOT NULL CHECK (phone != ''),
+  UNIQUE ("name", "adress", "email", "phone")
+);
+/*  */
 CREATE TABLE "contracts"(
-  contract_id serial PRIMARY KEY,
+  "contract_id" serial PRIMARY KEY,
   "contract_makeAt" timestamp NOT NULL DEFAULT current_timestamp,
-  "customerId" REFERENCES "customer"(id)
+  "customerId" INT REFERENCES "customer"(id),
+  UNIQUE ("contract_makeAt")
 );
-
+/*  */
 CREATE TABLE "products"(
-  code_id serial PRIMARY KEY,
-  name_product varchar(128) NOT NULL,
-  description varchar(256) NOT NULL,
-  price decimal(10, 2) NOT NULL CHECK (price > 0),
-  quantity int NOT NULL CHECK (quantity > 0),
-  UNIQUE(name_product, description)
+  "code_id" serial PRIMARY KEY,
+  "name_product" varchar(128) NOT NULL,
+  "description" varchar(256) NOT NULL,
+  "price" decimal(10, 2) NOT NULL CHECK (price > 0),
+  "av_quantity" int NOT NULL CHECK (av_quantity > 0),
+  UNIQUE (name_product, description)
 );
-
-CREATE TABLE "orders"(
-  order_id serial PRIMARY KEY,
-  "customerId" REFERENCES "customer"(id),
-  "customerAdress" REFERENCES "customer"(adress),
-  "customerPhone" REFERENCES "customer"(phone),
+/*  */
+CREATE TABLE "cast_orders"(
+  "order_id" serial PRIMARY KEY,
+  "contractId" INT REFERENCES "contracts" ("contract_id"),
+  "productId" INT REFERENCES "products"("code_id"),
+  "plan_quantity" int NOT NULL CHECK (plan_quantity > 0),
   "order_createdAt" timestamp NOT NULL DEFAULT current_timestamp
 );
-
+/*  */
 CREATE TABLE "shipment"(
-  shipment_id serial PRIMARY KEY,
-  "orderId_to" REFERENCES "orders"(order_id),
+  "shipment_id" serial PRIMARY KEY,
+  "orderId_to" INT REFERENCES "cast_orders"(order_id),
   "shipment_createdAt" timestamp NOT NULL DEFAULT current_timestamp,
-  ship_quantity int NOT NULL CHECK (ship_quantity > 0)
+  "ship_quantity" int NOT NULL CHECK (ship_quantity > 0)
 );
-
 /*  */
 CREATE TABLE "products_to_orders"(
   "productId" int REFERENCES "products"(code_id),
-  "orderId" int REFERENCES orders"(order_id),
-  PRIMARY KEY ("productId","orderId")
-)
+  "orderId" int REFERENCES "cast_orders"(order_id),
+  PRIMARY KEY ("productId", "orderId")
+);
