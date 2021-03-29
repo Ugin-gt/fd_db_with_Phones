0bec9857141542089e1b53730fdd23e6829f4860
Ugin-gt <apach575@gmail.com>
2021-03-29 03:09:34 +0300

    Restruct db shipment

--- fd_db_shipment.sql
+++ fd_db_shipment.sql
@@ -1,18 +1,23 @@
+create DATABASE shipment;
+/*  */
 CREATE TABLE "customer"(
   "id" serial PRIMARY KEY,
   "name" varchar(128) NOT NULL,
   "adress" JSONB NOT NULL,
   "email" varchar(256) NOT NULL CHECK (email != ''),
   "phone" char(11) NOT NULL CHECK (phone != ''),
-  UNIQUE ("name", "adress", "email", "phone")
+  UNIQUE ("adress", "email", "phone")
 );
 /*  */
 CREATE TABLE "contracts"(
   "contract_id" serial PRIMARY KEY,
-  "contract_makeAt" timestamp NOT NULL DEFAULT current_timestamp,
   "customerId" INT REFERENCES "customer"(id),
-  UNIQUE ("contract_makeAt")
+  "plan_quantity" INT NOT NULL CHECK (plan_quantity > 0),
+  "contract_makeAt" timestamp NOT NULL DEFAULT current_timestamp
 );
+Insert INTO "contracts" ("contract_id", "customerId", "plan_quantity")
+VALUES (1, 10, 100),
+  (2, 11, 200);
 /*  */
 CREATE TABLE "products"(
   "code_id" serial PRIMARY KEY,
@@ -20,26 +25,31 @@ CREATE TABLE "products"(
   "description" varchar(256) NOT NULL,
   "price" decimal(10, 2) NOT NULL CHECK (price > 0),
   "av_quantity" int NOT NULL CHECK (av_quantity > 0),
-  UNIQUE (name_product, description)
+  UNIQUE ("name_product", "description")
 );
 /*  */
 CREATE TABLE "cast_orders"(
   "order_id" serial PRIMARY KEY,
   "contractId" INT REFERENCES "contracts" ("contract_id"),
   "productId" INT REFERENCES "products"("code_id"),
-  "plan_quantity" int NOT NULL CHECK (plan_quantity > 0),
   "order_createdAt" timestamp NOT NULL DEFAULT current_timestamp
 );
+Insert INTO "cast_orders" ("order_id", "productId")
+VALUES (1, 10),
+  (2, 10);
 /*  */
 CREATE TABLE "shipment"(
   "shipment_id" serial PRIMARY KEY,
-  "orderId_to" INT REFERENCES "cast_orders"(order_id),
   "shipment_createdAt" timestamp NOT NULL DEFAULT current_timestamp,
   "ship_quantity" int NOT NULL CHECK (ship_quantity > 0)
 );
 /*  */
-CREATE TABLE "products_to_orders"(
-  "productId" int REFERENCES "products"(code_id),
+CREATE TABLE "orders_to_shipment"(
   "orderId" int REFERENCES "cast_orders"(order_id),
-  PRIMARY KEY ("productId", "orderId")
+  "shipmentId" int REFERENCES "shipment"(shipment_id)
+);
+/*  */
+CREATE TABLE "products_to_shipment"(
+  "productId" int REFERENCES "products"(code_id),
+  "shipmentId" int REFERENCES "shipment"(shipment_id)
 );
